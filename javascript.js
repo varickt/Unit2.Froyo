1a7712895338702bfcac1fed50af86db1d7ebe00
Varick Tecson <varicktecson@gmail.com>
2024-08-22 19:06:45 -0400

    updated 8.22.24

--- javascript.js
+++ javascript.js
@@ -2,12 +2,28 @@
 const flavors = prompt("Please enter a list of comma separated flavors");
 
 //Parse input into an array of froyo flavors
-const inputFlavors = flavors.split(",")
+const inputFlavors = flavors.split(",").map(flavor => flavor.trim());
 
 //create object to track which flavors you have observed so far
 const froyo = {};
 
-//loop of array to determine if flavor exists and increments count
-for (const key in inputFlavors){
-    console.log('');
+//looping through the array
+for (let i = 0; i < inputFlavors.length; i++){
+    const flavor = inputFlavors[i];
+
+    // check if the flavor exists in the object
+    if (froyo[flavor]){
+         // if it does exist, increase value by 1
+         froyo[flavor]++;
+    } else {
+        // if it doesn't exist, create the key and set value to 1
+        froyo[flavor] = 1;
+    }
 }
+   // return object
+   console.log(froyo);
+
+
+   
+    
+
